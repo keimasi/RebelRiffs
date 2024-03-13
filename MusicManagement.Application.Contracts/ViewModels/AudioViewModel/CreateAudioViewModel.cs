e669d91529ede8942e082b0cf828330b90e929c6
alireza-keimasi <keimasi.soft@gmail.com>
2024-03-13 18:46:44 +0330

    Implementation File Upload

--- MusicManagement.Application.Contracts/ViewModels/AudioViewModel/CreateAudioViewModel.cs
+++ MusicManagement.Application.Contracts/ViewModels/AudioViewModel/CreateAudioViewModel.cs
@@ -5,7 +5,6 @@ namespace MusicManagement.Application.Contracts.ViewModels.AudioViewModel;
 public class CreateAudioViewModel
 {
     public string Title { get; set; }
-    public IFormFile File { get; set; }
+    public IFormFile MusicFile { get; set; }
     public long AlbumId { get; set; }
-
 }
---
+++ ServiceHost/FileUpload.cs
@@ -0,0 +1,31 @@
+﻿using Framework.Application;
+
+namespace ServiceHost
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public string Upload(IFormFile file, string path)
+        {
+            if (file == null) return "";
+
+            var directoryPath = $"{_webHostEnvironment.WebRootPath}//Uploads//{path}";
+
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            var fileName = $"{DateTime.Now.ToFileName()}-{file.FileName}";
+            var filePath = $"{directoryPath}//{fileName}";
+            using var output = File.Create(filePath);
+            file.CopyTo(output);
+            return $"{path}/{fileName}";
+        }
+    }
+}
