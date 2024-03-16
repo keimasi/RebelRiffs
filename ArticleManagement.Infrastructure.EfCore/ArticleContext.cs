cb6c0e81a839922ffd676360b5bed2abfedd33d1
Brok <brokdeveloper@gmail.com>
2024-03-16 16:12:57 +0330

    add Infrastructure articel

---
+++ ArticleManagement.Infrastructure.EfCore/ArticleContext.cs
@@ -0,0 +1,21 @@
+﻿using ArticleManagement.Domain.Entity;
+using ArticleManagement.Infrastructure.EfCore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticleManagement.Infrastructure.EfCore
+{
+    public class ArticleContext : DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> context) : base(context) { }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var mapping = typeof(ArticleMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(mapping);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ArticleManagement.Infrastructure.EfCore/Mapping/ArticleMapping.cs
@@ -0,0 +1,18 @@
+﻿using ArticleManagement.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArticleManagement.Infrastructure.EfCore.Mapping;
+
+public class ArticleMapping : IEntityTypeConfiguration<Article>
+{
+    public void Configure(EntityTypeBuilder<Article> builder)
+    {
+        builder.ToTable("Articles");
+        builder.HasKey(e => e.Id);
+
+
+        builder.HasMany(e => e.Tags).WithOne(e => e.Article).HasForeignKey(e => e.ArticleId);
+
+    }
+}
